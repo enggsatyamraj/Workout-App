4a8037fbea27bf81bb8b28757fccb6f0ce8e4146
Satreena <118035430+Satreena@users.noreply.github.com>
2023-04-07 10:53:06 +0530

    made some new files , Work is in Progress now..

---
+++ app/src/main/java/com/example/homeworkout/jumping_jack_beginner.java
@@ -0,0 +1,14 @@
+package com.example.homeworkout;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class jumping_jack_beginner extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_jumping_jack_beginner);
+    }
+}
